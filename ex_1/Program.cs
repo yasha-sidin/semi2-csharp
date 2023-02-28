432338f1023ef75b3eae71d44576c21622698484
Yasha <yashasidin333@yandex.ru>
2023-02-28 12:56:56 +0300

    New update ex_1

--- ex_1/Program.cs
+++ ex_1/Program.cs
@@ -1,3 +1,16 @@
 ﻿// вывести случайное число из отрезка [10, 99]
 int number = new Random().Next(10, 100);
+
+int firstDigit = number / 10;
+int secondDigit = number % 10;
+
+if (firstDigit > secondDigit)
+{
+    Console.WriteLine(firstDigit);
+}
+else
+{
+    Console.WriteLine(secondDigit);
+}
+
 Console.WriteLine($"Слуайное число из отрезка [10, 99]: {number}");
