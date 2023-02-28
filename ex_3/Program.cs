d898bcb470157e317e07aff19d2976e4f8a9fc2a
Yasha <yashasidin333@yandex.ru>
2023-02-28 13:15:59 +0300

    Add ex_3

---
+++ ex_3/Program.cs
@@ -0,0 +1,15 @@
+﻿// На вход два числа, вывод: является ли второе кратным первому
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+
+if (a % b == 0)
+{
+    Console.WriteLine($"{a} кратно {b}");
+}
+else
+{
+    Console.WriteLine($"{a} не кратно {b}, остаток {a % b}");
+}
