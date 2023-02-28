19f8b7011b0ec6960e01810034ad4705454dfd43
Yasha <yashasidin333@yandex.ru>
2023-02-28 13:10:12 +0300

    Add ex_2

---
+++ ex_2/Program.cs
@@ -0,0 +1,9 @@
+﻿// вывод случайного трехзначного числа и удаляет вторую цифру этого числа
+int number = new Random().Next(100, 1000);
+
+Console.WriteLine($"Рандомное число: {number}");
+
+int result = (number / 100 * 10) + (number % 10);
+
+
+ Console.WriteLine($"Ответ: {result}");
