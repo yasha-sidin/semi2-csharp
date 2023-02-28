88fc9fefbfc8b61665a260029255990fb3349216
Yasha <yashasidin333@yandex.ru>
2023-02-28 13:38:55 +0300

    Add all

--- ex_2/Program.cs
+++ ex_2/Program.cs
@@ -1,9 +1,16 @@
 ﻿// вывод случайного трехзначного числа и удаляет вторую цифру этого числа
-int number = new Random().Next(100, 1000);
+//int number = new Random().Next(100, 1000);
 
-Console.WriteLine($"Рандомное число: {number}");
+//Console.WriteLine($"Рандомное число: {number}");
 
-int result = (number / 100 * 10) + (number % 10);
+//int result = (number / 100 * 10) + (number % 10);
 
 
- Console.WriteLine($"Ответ: {result}");
+//Console.WriteLine($"Ответ: {result}");
+
+string n = (new Random().Next(100, 1000)).ToString();
+
+Console.WriteLine($"Сгенерированное число: {n}");
+
+Console.Write(n[0]);
+Console.Write(n[2]);
---
+++ ex_4/Program.cs
@@ -0,0 +1,15 @@
+﻿// ввод числа, проверка кратно ли одновременно 7 и 23
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+//if (a % 7 == 0 && a % 23 == 0)
+//{
+//    Console.Write("Yes");
+//}
+//else
+//{
+//    Console.Write("No");
+//}
+
+bool result = a % 7 == 0 && a % 23 == 0;
+
+Console.WriteLine(result);
